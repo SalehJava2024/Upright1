d0eadbb885565fcc4e6c31e1ce0de72dc34aac6f
SalehJava2024 <ahmed.saleh0420@gmail.com>
2024-11-30 22:50:07 -0500

    second commit added if else practice class

---
+++ src/conditionalStatement/IfElasePractice1.java
@@ -0,0 +1,53 @@
+package conditionalStatement;
+
+public class IfElasePractice1 {
+
+    public static void ifelsePractice(){
+
+     int age =25;
+
+     if (age==10){
+    System.out.println("age is 10");
+     }
+
+     else if(age==20){
+
+   System.out.println("age is 20");
+
+     }
+
+    else if (age==25){
+     System.out.println("age is 25");
+
+     }
+
+     else {
+         System.out.println("none of the above is true");
+
+     }
+
+
+     }
+
+    public static void main(String[] args) {
+
+        IfElasePractice1 bbj = new IfElasePractice1();
+
+        //bbj.ifelsePractice();
+
+        ifelsePractice();
+
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
